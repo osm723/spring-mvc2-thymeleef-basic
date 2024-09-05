b756f3324ecbe1145fb3df955fa798b35f63344c
Oh <osm723@gmail.com>
2024-09-05 16:32:38 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 2. 타임리프 - 기본 기능 정리

---
+++ src/main/java/hello/thymeleef/ThymeleefBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleefBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleefBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymeleef/ThymeleefBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleefBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
